73642d0c24cf38a08456185b8977e1c9fc0cbc16
Alan Kent <ajkent@google.com>
2022-02-15 09:30:09 -0800

    Removed MUI 4.X from project

--- components/MUICookieConsent.js
+++ components/MUICookieConsent.js
@@ -7,13 +7,13 @@
 /* eslint-disable no-unused-vars */
 import * as React from 'react';
 import Cookies from 'js-cookie';
-import Snackbar from '@material-ui/core/Snackbar';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
+import Snackbar from '@mui/material/Snackbar';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 var window;
 var document;
