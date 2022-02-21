ac23ac7da88eb888d76df1d1cedb5e9f522bebbc
Alan Kent <alan.james.kent@gmail.com>
2022-02-21 09:24:08 -0800

    First cut at new course on building web apps

---
+++ pages/course/wa001/index.js
@@ -0,0 +1,15 @@
+import CoursePageLayout from '../../../components/CoursePageLayout'
+import Typography from '@mui/material/Typography'
+import Link from 'next/link'
+
+export default function Course_ch001() {
+  return (
+    <CoursePageLayout id="wa001">
+      
+      <Typography variant="h5" align="center" color="text.secondary" paragraph>
+        
+      </Typography>
+
+    </CoursePageLayout>
+  );
+}
